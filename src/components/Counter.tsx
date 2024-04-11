779b3b1d48dbe8d4fdb8c9fbc950239981673aa6
Anakor Joshua <backend.mern@gmail.com>
2024-04-11 10:21:37 +0100

    Import counter component in react app; Connect redux with react app

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../state/store';
+import { decrement, incrementByAmount } from '../state/counter/counterSlice';
+
 function Counter() {
-  return <div>Counter</div>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button className='btn' onClick={() => dispatch(incrementByAmount(10))}>
+        Increment
+      </button>
+      <button className='btn' onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
